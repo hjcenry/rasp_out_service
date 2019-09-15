d62ce789092e1351a80b3fe9032b20681b8095ec
hjcenry <359583129@qq.com>
2019-09-15 21:39:27 +0800

    init

---
+++ jjz/views.py
@@ -0,0 +1,16 @@
+import os
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def get_status(request):
+    invoke_shell = "python3 /home/pi/Soft/python/enterBjNotify/launch.py 1 4"
+    result = os.system(invoke_shell)
+    render_str = ''
+    if result is 0:
+        render_str = "已发送进京证信息到微信"
+    else:
+        render_str = "系统错误, code %d" % result
+
+    return render(request, "jjz_status.html", {'render_str': render_str})
---
+++ raspi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RaspiConfig(AppConfig):
+    name = 'raspi'
